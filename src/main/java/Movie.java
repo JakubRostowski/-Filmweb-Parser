68d328197f74bc3ce6495e97c02124e3aec3ff97
JakubRostowski <rostowskijakub@gmail.com>
2021-09-13 10:13:51 +0200

    minor code cleanup

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -72,14 +72,6 @@ public class Movie {
         return countryOfOrigin;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     @Override
     public String toString() {
         return "Movie{" +
