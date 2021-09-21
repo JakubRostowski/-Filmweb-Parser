477145c6aea49168e5a6a2e7832769bab5295b43
JakubRostowski <rostowskijakub@gmail.com>
2021-09-21 08:36:33 +0200

    checking changes implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 import java.io.IOException;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
     private static final MovieRepository movieRepository = new MovieRepository();
+    private static final UserListener userListener = new UserListener();
 
     public static void main(String[] args) throws IOException {
-
-        UserListener userListener = new UserListener();
 
         boolean newExcelFormat;
 
@@ -19,11 +20,22 @@ public class Main {
 
         System.out.println("Downloading the data from Filmweb.pl...");
         Map<Integer, Movie> movieMap = movieRepository.getTopList();
-        System.out.println("Looking for differences...");
+
+//        System.out.println("Exporting the data to database...");
+//        movieRepository.addToDatabase(movieMap);
+
         // check what to add and what to update
 
-        System.out.println("Exporting the data to database...");
-        movieRepository.addToDatabase(movieMap);
+        System.out.println("Looking for differences...");
+        int changedRecords = 0;
+        for (Map.Entry<Integer, Movie> movie : movieMap.entrySet()) {
+            if (!(movie.getValue().hashCode() == movieRepository.findById(movie.getKey()).hashCode())) {
+                System.out.println(movie.getValue().getPosition() + ". " + movie.getValue().getTitle() + " changed.");
+                changedRecords++;
+            }
+        }
+        System.out.println("Movies changed: " + changedRecords);
+
         System.out.println("Exporting the data to excel format...");
         movieRepository.exportToExcel(movieMap, newExcelFormat);
         System.out.println("Done!");
