3bd6cd2ffdb371c01a3057c1fad2b472a2d81056
JakubRostowski <rostowskijakub@gmail.com>
2021-09-23 10:49:24 +0200

    repository for ArchivedMovie added

--- src/main/java/ArchivedMovieRepository.java
+++ src/main/java/ArchivedMovieRepository.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ArchivedMovieRepository {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class ArchivedMovieRepository {
+
+    private static EntityManager em;
+
+    public ArchivedMovieRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public static void addArchivedMovie(ArchivedMovie movie) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(movie);
+        transaction.commit();
+    }
 }
