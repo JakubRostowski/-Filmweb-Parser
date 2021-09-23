0d2857c7d59b0221e01c60eea0e3339d2acc056f
JakubRostowski <rostowskijakub@gmail.com>
2021-09-23 10:50:21 +0200

    MovieService created

--- src/main/java/MovieService.java
+++ src/main/java/MovieService.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class MovieService {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class MovieService {
+    private static final EntityManagerFactory factory =
+            Persistence.createEntityManagerFactory("thePersistenceUnit");
+    private static final EntityManager em = factory.createEntityManager();
+
+    private static final MovieRepository movieRepository = new MovieRepository(em);
+    private static final ArchivedMovieRepository archievedMovieRepository = new ArchivedMovieRepository(em);
+
+    public static Map<Integer, Movie> downloadData() throws IOException {
+        System.out.println("Downloading the data from Filmweb.pl...");
+        return movieRepository.getTopList();
+    }
+
+    public static boolean populateDatabaseIfEmpty(Map<Integer, Movie> movieMap) {
+        if (movieRepository.checkIfEmpty()) {
+            System.out.println("Populating empty database...");
+            System.out.println("Database is empty, so I am adding movie Map to DB");
+            movieRepository.createDatabase(movieMap);
+            return true;
+        } else {
+            System.out.println("Database is not empty...");
+            return false;
+        }
+    }
+
+    public static void checkDifferences(Map<Integer, Movie> movieMap) {
+        System.out.println("Looking for differences...");
+
+        List<Movie> databaseMovies = MovieRepository.getMoviesFromDatabase();
+        for (Map.Entry<Integer, Movie> movie : movieMap.entrySet()) {
+            Movie checkedMovie = databaseMovies.get(movie.getValue().getPosition()-1);
+            if (movie.getValue().hashCode() == checkedMovie.hashCode()) {
+                MovieRepository.updateTimeOfModification(checkedMovie);
+            } else {
+                System.out.println(checkedMovie.getPosition() + ". " + checkedMovie.getTitle() + " changed.");
+                MovieRepository.deleteMovie(checkedMovie);
+                ArchivedMovieRepository.addArchivedMovie(checkedMovie.getArchivedMovie());
+            }
+        }
+    }
+
+    public static void ExportFile(Map<Integer, Movie> movieMap, boolean newExcelFormat) throws IOException {
+        System.out.println("Exporting the data to excel format...");
+        movieRepository.exportToExcel(movieMap, newExcelFormat);
+    }
+
 }
